7a22b62051298f25f032854b220c98cd6b6aa850
laierkasten23 <lia.schmid98@gmx.de>
2024-08-25 12:50:45 +0200

    compute affine matrix

--- Code_general_functions/compute_affine_matrix.py
+++ Code_general_functions/compute_affine_matrix.py
@@ -77,13 +77,49 @@ def get_nifti_by_modality(root_dir, image_type):
     image_paths = glob.glob(os.path.join(root_dir, '*', pattern))
     return [path for path in image_paths if os.path.basename(path).count('_') == 1] 
 
+def find_corresponding_t1(flair_image_paths, root_dir_t1):
+    flair_to_t1 = {}
+    for flair_path in flair_image_paths:
+        # Extract subject identifier from FLAIR filename
+        filename = os.path.basename(flair_path)
+        subject_id = filename.split('_')[0][2:]  # Remove 'rr' prefix and get the identifier
+        
+        # Construct the expected T1 path
+        t1_path = os.path.join(root_dir_t1, subject_id, f'{subject_id}_T1.nii')
+        
+        # Check if the T1 file exists
+        if os.path.exists(t1_path):
+            flair_to_t1[flair_path] = t1_path
+        else:
+            print(f'T1 image not found for subject {subject_id}')
+    
+    return flair_to_t1
+
 root_dir = '/home/linuxlia/Lia_Masterthesis/data/pazienti'
+root_dir_flair = '/home/linuxlia/Lia_Masterthesis/data/rrFLAIR_OK'
+root_dir_t1 = '/home/linuxlia/Lia_Masterthesis/data/pazienti'
+
+
 image_type = 'FLAIR'  # Change to 'T1' or other types as needed
+# just list all the nifti files in the directory rrFLAIR_OK
+flair_images_rr = sorted(glob.glob(os.path.join(root_dir_flair, '*.nii')))
+print("flair_images_rr = ", flair_images_rr)
+
+# Find corresponding T1 images
+flair_to_t1 = find_corresponding_t1(flair_images_rr, root_dir_t1)
+
+#flair_images = sorted(get_nifti_by_modality(root_dir, image_type))
 flair_images = sorted(get_nifti_by_modality(root_dir, image_type))
 t1_images = sorted(get_nifti_by_modality(root_dir, 'T1'))
 
-print("flair_images = ", flair_images)
-print("t1_images = ", t1_images)
+# Print the matched FLAIR and T1 image paths
+#for flair_path, t1_path in flair_to_t1.items():
+#    print(f'FLAIR: {flair_path} -> T1: {t1_path}')
+
+
+#print("flair_images = ", flair_images)
+#print("flair_images_rr = ", flair_images_rr)
+#print("t1_images = ", t1_images)
 
 #affine_flair1 = compute_affine_matrix(flair_images[26])
 #affine_t11 = compute_affine_matrix(t1_images[26])
@@ -107,7 +143,7 @@ for i in range(len(flair_images)):
 
 #print("flair1vst11 = ", flairvst1_104)
 
-#print(list_comparing_flair_vs_t1)
+print(list_comparing_flair_vs_t1)
 
 #t1_38_path = '/home/linuxlia/Lia_Masterthesis/data/pazienti1_52_Lia/038/038_T1.nii' 
 #flair_38_path = '/home/linuxlia/Lia_Masterthesis/data/pazienti1_52_Lia/038/038_FLAIR.nii'
@@ -117,5 +153,22 @@ for i in range(len(flair_images)):
 #t1_vs_flair_38_coregistred = compare_affine_matrices(t1_38_path, flair_38_coregistred_path)
 #print("t1_vs_flair_38 = ", t1_vs_flair_38)
 #print("t1_vs_flair_38_coregistred = ", t1_vs_flair_38_coregistred)
-print(list_comparing_flair_vs_t1)
-print(list_flair_affine[87] - list_t1_affine[87])
+##print(list_comparing_flair_vs_t1)
+#print(list_flair_affine[87] - list_t1_affine[87])
+
+
+# Example usage of the matched paths
+list_comparing_flair_vs_t1 = []
+list_flair_affine = []
+list_t1_affine = []
+for (flair_path, t1_path) in flair_to_t1.items():
+    print(i)
+    checker_flair_vs_t1, affine_flair, affine_t1 = compare_affine_matrices(flair_path, t1_path)
+    list_flair_affine.append(affine_flair)
+    list_t1_affine.append(affine_t1)
+    if checker_flair_vs_t1:
+        list_comparing_flair_vs_t1.append(flair_path + " check")
+    else:
+        list_comparing_flair_vs_t1.append(" affine matrix NOT equal")
+
+print("list_comparing", list_comparing_flair_vs_t1)
